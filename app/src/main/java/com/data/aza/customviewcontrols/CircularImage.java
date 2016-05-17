4669843130e0726ef6ec6cf7334bf4151bfdc2dd
oda <sher.ali@ondemandagility.com>
2016-05-17 15:40:52 +0530

    Log added

---
+++ app/src/main/java/com/data/aza/customviewcontrols/CircularImage.java
@@ -0,0 +1,7 @@
+package com.data.aza.customviewcontrols;
+
+/**
+ * Created by oda on 17/05/16.
+ */
+public class CircularImage {
+}
--- app/src/main/java/com/data/aza/customviewcontrols/MainActivity.java
+++ app/src/main/java/com/data/aza/customviewcontrols/MainActivity.java
@@ -5,25 +5,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = MainActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        // New file linked in repository
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Place your action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this,"I am a Toast :)",Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+                Log.d(TAG, "Replace Action");
             }
         });
     }
